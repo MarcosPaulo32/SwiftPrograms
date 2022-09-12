36dfef169f181cc7d158bf704fbc42a00cb2763c
ICMMAC04-3F86 <icmmac04-3f86@ICMMAC04-3F86s-Mac-mini.local>
2022-09-12 19:11:11 -0400

    Jogo da Velha refeito + Constrain no myname

--- jogodavelha/jogodavelha/ViewController.swift
+++ jogodavelha/jogodavelha/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
     // Cada bloco no jogo da velha
+    @IBOutlet weak var X: UIButton!
+    @IBOutlet weak var O: UIButton!
     @IBOutlet weak var xo1: UIButton!
     @IBOutlet weak var xo2: UIButton!
     @IBOutlet weak var xo3: UIButton!
@@ -31,15 +33,135 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     @IBAction func X(_ sender: Any) {
-       Choose = 1
+       Choose = 2
         jogodavelha.isHidden = true
         
     }
     @IBAction func Bola(_ sender: Any) {
-        Choose = 2
+        Choose = 1
         jogodavelha.isHidden = true
+    }
+    @IBAction func XO1(_ sender: Any) {
+        if Choose == 1 {
+            xo1.setBackgroundImage(UIImage(named: "circulozinho"), for: .normal)
+            Choose = 2
+            xo1.isUserInteractionEnabled = false
+        }else{
+            xo1.setBackgroundImage(UIImage(named: "xiszinho"), for: .normal)
+            Choose = 1
+            xo1.isUserInteractionEnabled = false
+        }
+    }
+    @IBAction func XO2(_ sender: Any) {
+        if Choose == 1 {
+            xo2.setBackgroundImage(UIImage(named: "circulozinho"), for: .normal)
+            Choose = 2
+            xo2.isUserInteractionEnabled = false
+        }else{
+            xo2.setBackgroundImage(UIImage(named: "xiszinho"), for: .normal)
+            Choose = 1
+            xo2.isUserInteractionEnabled = false
+        }
+    }
+    @IBAction func XO3(_ sender: Any) {
+        if Choose == 1 {
+            xo3.setBackgroundImage(UIImage(named: "circulozinho"), for: .normal)
+            Choose = 2
+            xo3.isUserInteractionEnabled = false
+        }else{
+            xo3.setBackgroundImage(UIImage(named: "xiszinho"), for: .normal)
+            Choose = 1
+            xo3.isUserInteractionEnabled = false
+        }
+    }
+    @IBAction func XO4(_ sender: Any) {
+        if Choose == 1 {
+            xo4.setBackgroundImage(UIImage(named: "circulozinho"), for: .normal)
+            Choose = 2
+            xo4.isUserInteractionEnabled = false
+        }else{
+            xo4.setBackgroundImage(UIImage(named: "xiszinho"), for: .normal)
+            Choose = 1
+            xo4.isUserInteractionEnabled = false
+        }
+    }
+    @IBAction func XO5(_ sender: Any) {
+        if Choose == 1 {
+            xo5.setBackgroundImage(UIImage(named: "circulozinho"), for: .normal)
+            Choose = 2
+            xo5.isUserInteractionEnabled = false
+        }else{
+            xo5.setBackgroundImage(UIImage(named: "xiszinho"), for: .normal)
+            Choose = 1
+            xo5.isUserInteractionEnabled = false
+        }
+    }
+    @IBAction func XO6(_ sender: Any) {
+        if Choose == 1 {
+            xo6.setBackgroundImage(UIImage(named: "circulozinho"), for: .normal)
+            Choose = 2
+            xo6.isUserInteractionEnabled = false
+        }else{
+            xo6.setBackgroundImage(UIImage(named: "xiszinho"), for: .normal)
+            Choose = 1
+            xo6.isUserInteractionEnabled = false
+        }
+    }
+    @IBAction func XO7(_ sender: Any) {
+        if Choose == 1 {
+            xo7.setBackgroundImage(UIImage(named: "circulozinho"), for: .normal)
+            Choose = 2
+            xo7.isUserInteractionEnabled = false
+        }else{
+            xo7.setBackgroundImage(UIImage(named: "xiszinho"), for: .normal)
+            Choose = 1
+            xo7.isUserInteractionEnabled = false
+        }
+    }
+    @IBAction func XO8(_ sender: Any) {
+        if Choose == 1 {
+            xo8.setBackgroundImage(UIImage(named: "circulozinho"), for: .normal)
+            Choose = 2
+            xo8.isUserInteractionEnabled = false
+        }else{
+            xo8.setBackgroundImage(UIImage(named: "xiszinho"), for: .normal)
+            Choose = 1
+            xo8.isUserInteractionEnabled = false
+        }
+    }
+    @IBAction func XO9(_ sender: Any) {
+        if Choose == 1 {
+            xo9.setBackgroundImage(UIImage(named: "circulozinho"), for: .normal)
+            Choose = 2
+            xo9.isUserInteractionEnabled = false
+        }else{
+            xo9.setBackgroundImage(UIImage(named: "xiszinho"), for: .normal)
+            Choose = 1
+            xo9.isUserInteractionEnabled = false
+        }
+    }
+    @IBAction func CloseButton(_ sender: Any) {
+        jogodavelha.isHidden = false
+                xo1.setBackgroundImage(UIImage(named: ""), for: .normal)
+                xo1.isUserInteractionEnabled = true
+                xo2.setBackgroundImage(UIImage(named: ""), for: .normal)
+                xo2.isUserInteractionEnabled = true
+                xo3.setBackgroundImage(UIImage(named: ""), for: .normal)
+                xo3.isUserInteractionEnabled = true
+                xo4.setBackgroundImage(UIImage(named: ""), for: .normal)
+                xo4.isUserInteractionEnabled = true
+                xo5.setBackgroundImage(UIImage(named: ""), for: .normal)
+        xo5.isUserInteractionEnabled = true
+                xo6.isUserInteractionEnabled = true
+                xo6.setBackgroundImage(UIImage(named: ""), for: .normal)
+        xo7.setBackgroundImage(UIImage(named: ""), for: .normal)
+                xo7.isUserInteractionEnabled = true
+                xo8.setBackgroundImage(UIImage(named: ""), for: .normal)
+        xo8.isUserInteractionEnabled = true
+                xo9.isUserInteractionEnabled = true
+                xo9.setBackgroundImage(UIImage(named: ""), for: .normal)
+    }
     }
     
 
-}
 
